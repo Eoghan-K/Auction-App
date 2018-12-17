f8a9d46c64f457fdeb67c0b410a14b376f25d967
Chris Garvin <chrisgarvin97@gmail.com>
2018-12-17 17:42:18 +0000

    about page

--- about.php
+++ about.php
@@ -2,6 +2,55 @@
     include "includes/header.php";
 ?>
 
+<div class="ct-pageWrapper" id="ct-js-wrapper">
+    <section class="company-heading intro-type" id="parallax-one">
+        <div class="container">
+            <div class="row product-title-info">
+                <div class="col-md-12">
+                    <h1>About Us</h1>
+                </div>
+            </div>
+        </div>
+        <div class="parallax" id="parallax-cta" style="background-image:url(https://www.solodev.com/assets/hero/hero.jpg);"></div>
+    </section>
+    <section class="story-section company-sections ct-u-paddingBoth100 paddingBothHalf noTopMobilePadding" id="section">
+        <div class="container text-center">
+            <h1>WHAT IS TOPCHOICEAUCTIONS?</h1>
+            <div class="col-md-8 col-md-offset-2">
+                <div class="red-border"></div>
+                <p class="ct-u-size22 ct-u-fontWeight300 marginTop40">
+                    TopChoice Auctions is an application built to hold auctions created by
+                    users, as a safe and easy way to buy and sell items. We also have a section
+                    dedicated to buy items without having to bid on them. People selling things
+                    sometimes have a set price that they want, if someone wants to buy the item
+                    for that specific price it saves a lot of time and effort.
+                    If the seller wants to, he/she can also create an auction for the item, if it
+                    doesn't sell at the wanted price. This could also technically get you more than
+                    the original wanted price, but it is more of a gamble then.
+                </p>
+                <!-- <a class="ct-u-marginTop60 btn btn-solodev-red btn-fullWidth-sm ct-u-size19" href="#">Learn More</a> -->
+            </div>
+        </div>
+    </section>
+    <section class="culture-section company-sections ct-u-paddingBoth100 paddingBothHalf noTopMobilePadding">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-8 col-md-offset-2">
+                    <h2>Customer Support</h2>
+                    <p class="ct-u-size22 ct-u-fontWeight300 ct-u-marginBottom60">
+                        Here at TopChoiceAuctions, we pride ourselves on having amazing customer
+                        service. We will make sure to fix any issues you may have in an efficient
+                        and correct manor. You can send us an email or fill out our form, we will
+                        make sure to email you within 3 working days.</p>
+                </div>
+            </div>
+
+            <a class="ct-u-marginTop60 btn btn-solodev-red-reversed btn-fullWidth-sm ct-u-size19" href="/contact.php">Contact us now!</a>
+    </section>
+
+
+
+
 <?php
     include "includes/bootstrapScripts.php";
 ?>
