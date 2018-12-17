9b5467410231a272702839da4a992b6bf306f01d
Eoghan <eoghankenny95@gmail.com>
2018-12-17 19:50:44 +0000

    index

--- item.php
+++ item.php
@@ -1,58 +1,67 @@
 <?php
     include "includes/header.php";
-    include "scripts/Item.php";
-    
-    use Item as I;
-    $item = new I(5);
-    $images = $item->getImages();
 ?>
-
-<div id="carouselExampleFade" class="carousel slide carousel-fade" data-ride="carousel">
-    <div class="carousel-inner">
-        <div class="carousel-item active">
-            <img class="d-block" src="images/image1.jpg" alt="First slide">
-        </div>
-        <div class="carousel-item">
-            <img class="d-block" src="images/image2.jpg" alt="Second slide">
-        </div>
-        <div class="carousel-item">
-            <img class="d-block" src="images/image3.jpg" alt="Third slide">
-        </div>
-    </div>
-    <a class="carousel-control-prev" href="#carouselExampleFade" role="button" data-slide="prev">
-        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-        <span class="sr-only">Previous</span>
-    </a>
-    <a class="carousel-control-next" href="#carouselExampleFade" role="button" data-slide="next">
-        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-        <span class="sr-only">Next</span>
-    </a>
-</div>
-
-<div class="container text-center">
-    <h3>Item Description</h3><br>
-    <div class="row">
-        <div class="col-sm-4">
-            <img src="" class="img-responsive" style="width:100%" alt="Image">
-            <p>Image 1</p>
-        </div>
-        <div class="col-sm-4">
-            <img src="" class="img-responsive" style="width:100%" alt="Image">
-            <p>Image 2</p>
-        </div>
-        <div class="col-sm-4">
-            <div class="well">
-                <p>descption</p>
+    
+    <div id="myCarousel" class="carousel slide" data-ride="carousel">
+        
+        <ol class="carousel-indicators">
+            <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+            <li data-target="#myCarousel" data-slide-to="1"></li>
+        </ol>
+        
+        
+        <div class="carousel-inner" role="listbox">
+            <div class="item active">
+                <img src=" source " alt="Image">
+                <div class="carousel-caption">
+                    <h3>Header 1</h3>
+                    <p>Sub Header 1.</p>
+                </div>
             </div>
-            <div class="well">
-                <p>descption </p>
+            
+            <div class="item">
+                <img src=" source" alt="Image">
+                <div class="carousel-caption">
+                    <h3> Header 2 </h3>h3>
+                    <p>Sub Header 2</p>
+                </div>
             </div>
         </div>
+        
+        <!-- Left and right controls -->
+        <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+            <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+        </a>
+        <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+            <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+        </a>
     </div>
-</div>
+    
+    <div class="container text-center">
+        <h3>Item Description</h3><br>
+        <div class="row">
+            <div class="col-sm-4">
+                <img src="" class="img-responsive" style="width:100%" alt="Image">
+                <p>Image 1</p>
+            </div>
+            <div class="col-sm-4">
+                <img src="" class="img-responsive" style="width:100%" alt="Image">
+                <p>Image 2</p>
+            </div>
+            <div class="col-sm-4">
+                <div class="well">
+                    <p>descption</p>
+                </div>
+                <div class="well">
+                    <p>descption </p>
+                </div>
+            </div>
+        </div>
+    </div>
 
 <?php
-    
     include "includes/bootstrapScripts.php";
 
 ?>
