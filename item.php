10dbd5cc23e3c190bec4f3e27510516144290c93
Eoghan <eoghankenny95@gmail.com>
2018-12-18 20:16:03 +0000

    item displaying kinda

--- item.php
+++ item.php
@@ -5,9 +5,9 @@
 	$item = null;
 	$images = null;
 	
-	if ( isset( $_GET[ "title" ] ) ) {
-		$title = $_GET[ "title" ];
-		$i = new Item( $title );
+	if ( isset( $_GET[ "id" ] ) ) {
+		$id = $_GET[ "id" ];
+		$i = new Item( $id );
 		$item = $i->getItem();
 		$images = $i->getImages();
 	} else {
@@ -16,60 +16,62 @@
 
 ?>
 	
-	<div id="myCarousel" class="carousel slide" data-ride="carousel">
-		
-		<ol class="carousel-indicators">
-			<li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-			<li data-target="#myCarousel" data-slide-to="1"></li>
-		</ol>
-		
-		
-		<div class="carousel-inner" role="listbox">
-			<div class="item active">
-				<img src=" source " alt="Image">
-				<div class="carousel-caption">
-					<h3>Header 1</h3>
-					<p>Sub Header 1.</p>
-				</div>
-			</div>
-			
-			<div class="item">
-				<img src=" source" alt="Image">
-				<div class="carousel-caption">
-					<h3> Header 2 </h3>h3>
-					<p>Sub Header 2</p>
-				</div>
-			</div>
-		</div>
-		
-		<!-- Left and right controls -->
-		<a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
-			<span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-			<span class="sr-only">Previous</span>
-		</a>
-		<a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
-			<span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-			<span class="sr-only">Next</span>
-		</a>
-	</div>
+<!--	<div id="myCarousel" class="carousel slide" data-ride="carousel">-->
+<!--		-->
+<!--		<ol class="carousel-indicators">-->
+<!--			<li data-target="#myCarousel" data-slide-to="0" class="active"></li>-->
+<!--			<li data-target="#myCarousel" data-slide-to="1"></li>-->
+<!--		</ol>-->
+<!--		-->
+<!--		-->
+<!--		<div class="carousel-inner" role="listbox">-->
+<!--			<div class="item active">-->
+<!--				<img src="images/uploaded/sales/--><?php //echo $images[ 0 ][ 'image_url' ] ?><!--" alt="Image">-->
+<!--				<div class="carousel-caption">-->
+<!--					<h3>Header 1</h3>-->
+<!--					<p>Sub Header 1.</p>-->
+<!--				</div>-->
+<!--			</div>-->
+<!--			-->
+<!--			<div class="item">-->
+<!--				<img src="images/uploaded/sales/--><?php //echo $images[ 1 ][ 'image_url' ] ?><!--" alt="Image">-->
+<!--				<div class="carousel-caption">-->
+<!--					<h3> Header 2 </h3>h3>-->
+<!--					<p>Sub Header 2</p>-->
+<!--				</div>-->
+<!--			</div>-->
+<!--		</div>-->
+<!--		-->
+
+<!--		<a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">-->
+<!--			<span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>-->
+<!--			<span class="sr-only">Previous</span>-->
+<!--		</a>-->
+<!--		<a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">-->
+<!--			<span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>-->
+<!--			<span class="sr-only">Next</span>-->
+<!--		</a>-->
+<!--	</div>-->
 	
 	<div class="container text-center">
-		<h3>Item Description</h3><br>
+		<h3><?php echo $item['item_name'] ?></h3><br>
 		<div class="row">
-			<div class="col-sm-4">
-				<img src="" class="img-responsive" style="width:100%" alt="Image">
-				<p>Image 1</p>
-			</div>
-			<div class="col-sm-4">
-				<img src="" class="img-responsive" style="width:100%" alt="Image">
-				<p>Image 2</p>
-			</div>
+			<?php  for ($j = 0; $j < sizeof($images); $j++){ ?>
+				<div class="col-sm-4">
+					<img src="images/uploaded/sales/<?php echo $images[ $j ][ 'image_url' ] ?>"
+					     class="img-responsive"
+					     style="width:100%"
+					     alt="Image"
+					>
+					<p><?php echo $images[ $j ][ 'image_name' ] ?></p>
+				</div>
+			<?php }?>
 			<div class="col-sm-4">
 				<div class="well">
-					<p>descption</p>
+					<p>Description</p>
 				</div>
 				<div class="well">
-					<p>descption </p>
+					<p><?php echo $item['item_description'] ?></p>
 				</div>
 			</div>
 		</div>
